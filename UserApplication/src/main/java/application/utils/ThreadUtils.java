bcc3f835d16162e7f73b627aa672f08f6a33dc00
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-12-05 22:41:33 +0100

    feat: create thread utils

---
+++ UserApplication/src/main/java/application/utils/ThreadUtils.java
@@ -0,0 +1,10 @@
+package application.utils;
+
+public final class ThreadUtils {
+
+    public static void sleep(final int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (final InterruptedException ignored) { }
+    }
+}
