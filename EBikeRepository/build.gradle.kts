5371f3f3eea4b13ab21235c1747425e715f8321d
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-12-06 18:16:12 +0100

    build: add spring in ebike repository

--- EBikeRepository/build.gradle.kts
+++ EBikeRepository/build.gradle.kts
@@ -10,6 +10,12 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.3.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+
+    implementation("com.google.code.gson:gson:2.11.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
