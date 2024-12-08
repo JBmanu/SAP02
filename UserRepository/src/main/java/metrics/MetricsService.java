369f8786d5614336654a8cb8d87191667deb5ca7
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-12-08 17:05:18 +0100

    refactor: label name of metrics service

--- UserRepository/src/main/java/metrics/MetricsService.java
+++ UserRepository/src/main/java/metrics/MetricsService.java
@@ -4,11 +4,21 @@ import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 
 public final class MetricsService {
+    // LABEL NAMES
     public static final String USERNAME = "username";
+    public static final String AMOUNT = "amount";
     public static final String STATUS = "status";
+
+    // STATUS VALUES
     public static final String SUCCESS = "success";
     public static final String FAILURE = "failure";
 
+    // COUNTER NAMES
+    public static final String USER_REGISTRATIONS_TOTAL = "user_registrations_total";
+    public static final String USER_LOGINS_TOTAL = "user_logins_total";
+    public static final String CREDITS_ADDED_TOTAL = "credits_added_total";
+    public static final String CREDITS_REMOVED_TOTAL = "credits_removed_total";
+    public static final String TOTAL_USER_CREDITS = "total_user_credits";
 
     private final Counter userRegistrations;
     private final Counter userLogins;
@@ -18,31 +28,31 @@ public final class MetricsService {
 
     public MetricsService() {
          this.userRegistrations = Counter.build()
-                 .name("user_registrations_total")
+                 .name(USER_REGISTRATIONS_TOTAL)
                  .help("Total number of user registrations.")
-                 .labelNames(USERNAME, STATUS)  // Aggiungi etichette per "user_id" e "status"
+                 .labelNames(USERNAME, STATUS)
                  .register();
 
         this.userLogins = Counter.build()
-                .name("user_logins_total")
+                .name(USER_LOGINS_TOTAL)
                 .help("Total number of user logins.")
-                .labelNames(USERNAME, STATUS)  // Etichette per "user_id" e "status"
+                .labelNames(USERNAME, STATUS)
                 .register();
 
         this.creditsAdded = Counter.build()
-                .name("credits_added_total")
+                .name(CREDITS_ADDED_TOTAL)
                 .help("Total number of credits added by users.")
-                .labelNames(USERNAME, "amount", STATUS)  // Etichette per l'utente, importo e stato
+                .labelNames(USERNAME, AMOUNT, STATUS)
                 .register();
 
         this.creditsRemoved = Counter.build()
-                .name("credits_removed_total")
+                .name(CREDITS_REMOVED_TOTAL)
                 .help("Total number of credits removed by users.")
-                .labelNames(USERNAME, "amount", STATUS)  // Etichette per l'utente, importo e stato
+                .labelNames(USERNAME, AMOUNT, STATUS)
                 .register();
 
         this.totalCredits = Gauge.build()
-                .name("total_user_credits")
+                .name(TOTAL_USER_CREDITS)
                 .help("Total number of credits across all users.")
                 .labelNames(USERNAME)  // Etichetta per l'utente
                 .register();
